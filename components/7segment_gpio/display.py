4b45c74aba42f7395fa90db19167af64c9f6596e
Anton Sergunov <setosha@gmail.com>
2024-03-09 05:56:12 +0000

    Don't register display

--- components/7segment_gpio/display.py
+++ components/7segment_gpio/display.py
@@ -43,8 +43,8 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    # await cg.register_component(var, config)
-    await display.register_display(var, config)
+    await cg.register_component(var, config)
+    # await display.register_display(var, config)
 
     cg.add(var.set_intensity(config[CONF_INTENSITY]))
     # cg.add(var.set_reverse(config[CONF_REVERSE_ENABLE]))
